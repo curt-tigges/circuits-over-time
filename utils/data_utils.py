2fc8510133345a93740d761790bd89376e2263cc
curt-tigges <ct@curttigges.com>
2024-01-08 08:24:12 -0800

    Added args to get_model_results

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -7,6 +7,9 @@ import numpy as np
 import pandas as pd
 import yaml
 
+from torch import Tensor
+from jaxtyping import Float
+
 import einops
 from fancy_einsum import einsum
 
@@ -38,6 +41,9 @@ from transformer_lens import (
     ActivationCache,
 )
 
+from path_patching_cm.path_patching import Node, IterNode, path_patch, act_patch
+from path_patching_cm.ioi_dataset import IOIDataset, NAMES
+
 from functools import partial
 
 from torchtyping import TensorType as TT
@@ -59,3 +65,67 @@ def read_data(file_path):
     ]  # Remove the last empty item
 
     return prompts, answers
+
+
+def _logits_to_ave_logit_diff(logits: Float[Tensor, "batch seq d_vocab"], ioi_dataset: IOIDataset, per_prompt=False):
+    '''
+    Returns logit difference between the correct and incorrect answer.
+
+    If per_prompt=True, return the array of differences rather than the average.
+    '''
+
+    # Only the final logits are relevant for the answer
+    # Get the logits corresponding to the indirect object / subject tokens respectively
+    io_logits: Float[Tensor, "batch"] = logits[range(logits.size(0)), ioi_dataset.word_idx["end"], ioi_dataset.io_tokenIDs]
+    s_logits: Float[Tensor, "batch"] = logits[range(logits.size(0)), ioi_dataset.word_idx["end"], ioi_dataset.s_tokenIDs]
+    # Find logit difference
+    answer_logit_diff = io_logits - s_logits
+    return answer_logit_diff if per_prompt else answer_logit_diff.mean()
+
+def _ioi_metric_noising(
+        logits: Float[Tensor, "batch seq d_vocab"],
+        clean_logit_diff: float,
+        corrupted_logit_diff: float,
+        ioi_dataset: IOIDataset,
+    ) -> float:
+        '''
+        We calibrate this so that the value is 0 when performance isn't harmed (i.e. same as IOI dataset),
+        and -1 when performance has been destroyed (i.e. is same as ABC dataset).
+        '''
+        patched_logit_diff = _logits_to_ave_logit_diff(logits, ioi_dataset)
+        return ((patched_logit_diff - clean_logit_diff) / (clean_logit_diff - corrupted_logit_diff)).item()
+
+
+def generate_data_and_caches(model: HookedTransformer, N: int, verbose: bool = False, seed: int = 42):
+
+    ioi_dataset = IOIDataset(
+        prompt_type="mixed",
+        N=N,
+        tokenizer=model.tokenizer,
+        prepend_bos=False,
+        seed=seed,
+        device=str(device)
+    )
+
+    abc_dataset = ioi_dataset.gen_flipped_prompts("ABB->ABA, BAB->BAA")
+
+    model.reset_hooks(including_permanent=True)
+
+    ioi_logits_original, ioi_cache = model.run_with_cache(ioi_dataset.toks)
+    abc_logits_original, abc_cache = model.run_with_cache(abc_dataset.toks)
+
+    ioi_average_logit_diff = _logits_to_ave_logit_diff(ioi_logits_original, ioi_dataset).item()
+    abc_average_logit_diff = _logits_to_ave_logit_diff(abc_logits_original, ioi_dataset).item()
+
+    if verbose:
+        print(f"Average logit diff (IOI dataset): {ioi_average_logit_diff:.4f}")
+        print(f"Average logit diff (ABC dataset): {abc_average_logit_diff:.4f}")
+
+    ioi_metric_noising = partial(
+        _ioi_metric_noising,
+        clean_logit_diff=ioi_average_logit_diff,
+        corrupted_logit_diff=abc_average_logit_diff,
+        ioi_dataset=ioi_dataset,
+    )
+
+    return ioi_dataset, abc_dataset, ioi_cache, abc_cache, ioi_metric_noising
