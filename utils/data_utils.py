27aed91ea00351923380f8e03daee9c80229e8f9
curt-tigges <ct@curttigges.com>
2024-01-08 15:56:21 -0800

    Added accuracy metric

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -43,6 +43,7 @@ from transformer_lens import (
 
 from path_patching_cm.path_patching import Node, IterNode, path_patch, act_patch
 from path_patching_cm.ioi_dataset import IOIDataset, NAMES
+from utils.metrics import _logits_to_mean_logit_diff, _ioi_metric_noising
 
 from functools import partial
 
@@ -67,33 +68,7 @@ def read_data(file_path):
     return prompts, answers
 
 
-def _logits_to_ave_logit_diff(logits: Float[Tensor, "batch seq d_vocab"], ioi_dataset: IOIDataset, per_prompt=False):
-    '''
-    Returns logit difference between the correct and incorrect answer.
 
-    If per_prompt=True, return the array of differences rather than the average.
-    '''
-
-    # Only the final logits are relevant for the answer
-    # Get the logits corresponding to the indirect object / subject tokens respectively
-    io_logits: Float[Tensor, "batch"] = logits[range(logits.size(0)), ioi_dataset.word_idx["end"], ioi_dataset.io_tokenIDs]
-    s_logits: Float[Tensor, "batch"] = logits[range(logits.size(0)), ioi_dataset.word_idx["end"], ioi_dataset.s_tokenIDs]
-    # Find logit difference
-    answer_logit_diff = io_logits - s_logits
-    return answer_logit_diff if per_prompt else answer_logit_diff.mean()
-
-def _ioi_metric_noising(
-        logits: Float[Tensor, "batch seq d_vocab"],
-        clean_logit_diff: float,
-        corrupted_logit_diff: float,
-        ioi_dataset: IOIDataset,
-    ) -> float:
-        '''
-        We calibrate this so that the value is 0 when performance isn't harmed (i.e. same as IOI dataset),
-        and -1 when performance has been destroyed (i.e. is same as ABC dataset).
-        '''
-        patched_logit_diff = _logits_to_ave_logit_diff(logits, ioi_dataset)
-        return ((patched_logit_diff - clean_logit_diff) / (clean_logit_diff - corrupted_logit_diff)).item()
 
 
 def generate_data_and_caches(model: HookedTransformer, N: int, verbose: bool = False, seed: int = 42):
@@ -114,8 +89,8 @@ def generate_data_and_caches(model: HookedTransformer, N: int, verbose: bool = F
     ioi_logits_original, ioi_cache = model.run_with_cache(ioi_dataset.toks)
     abc_logits_original, abc_cache = model.run_with_cache(abc_dataset.toks)
 
-    ioi_average_logit_diff = _logits_to_ave_logit_diff(ioi_logits_original, ioi_dataset).item()
-    abc_average_logit_diff = _logits_to_ave_logit_diff(abc_logits_original, ioi_dataset).item()
+    ioi_average_logit_diff = _logits_to_mean_logit_diff(ioi_logits_original, ioi_dataset).item()
+    abc_average_logit_diff = _logits_to_mean_logit_diff(abc_logits_original, ioi_dataset).item()
 
     if verbose:
         print(f"Average logit diff (IOI dataset): {ioi_average_logit_diff:.4f}")
---
+++ utils/metrics.py
@@ -0,0 +1,49 @@
+from torch import Tensor
+from jaxtyping import Float
+
+
+from path_patching_cm.ioi_dataset import IOIDataset
+
+
+def _logits_to_mean_logit_diff(logits: Float[Tensor, "batch seq d_vocab"], ioi_dataset: IOIDataset, per_prompt=False):
+    '''
+    Returns logit difference between the correct and incorrect answer.
+
+    If per_prompt=True, return the array of differences rather than the average.
+    '''
+
+    # Only the final logits are relevant for the answer
+    # Get the logits corresponding to the indirect object / subject tokens respectively
+    io_logits: Float[Tensor, "batch"] = logits[range(logits.size(0)), ioi_dataset.word_idx["end"], ioi_dataset.io_tokenIDs]
+    s_logits: Float[Tensor, "batch"] = logits[range(logits.size(0)), ioi_dataset.word_idx["end"], ioi_dataset.s_tokenIDs]
+    # Find logit difference
+    answer_logit_diff = io_logits - s_logits
+    return answer_logit_diff if per_prompt else answer_logit_diff.mean()
+
+
+def _logits_to_mean_accuracy(logits: Float[Tensor, "batch seq d_vocab"], ioi_dataset: IOIDataset):
+    '''
+    Returns accuracy of the model on the IOI dataset.
+    '''
+    # Only the final logits are relevant for the answer
+    # Get the logits corresponding to the indirect object / subject tokens respectively
+    io_logits: Float[Tensor, "batch"] = logits[range(logits.size(0)), ioi_dataset.word_idx["end"], ioi_dataset.io_tokenIDs]
+    s_logits: Float[Tensor, "batch"] = logits[range(logits.size(0)), ioi_dataset.word_idx["end"], ioi_dataset.s_tokenIDs]
+    # Find logit difference
+    answer_logit_diff = io_logits - s_logits
+    # Find accuracy
+    return (answer_logit_diff > 0).float().mean()
+
+
+def _ioi_metric_noising(
+        logits: Float[Tensor, "batch seq d_vocab"],
+        clean_logit_diff: float,
+        corrupted_logit_diff: float,
+        ioi_dataset: IOIDataset,
+    ) -> float:
+        '''
+        We calibrate this so that the value is 0 when performance isn't harmed (i.e. same as IOI dataset),
+        and -1 when performance has been destroyed (i.e. is same as ABC dataset).
+        '''
+        patched_logit_diff = _logits_to_mean_logit_diff(logits, ioi_dataset)
+        return ((patched_logit_diff - clean_logit_diff) / (clean_logit_diff - corrupted_logit_diff)).item()
