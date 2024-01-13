85caf49228b6c08e713be90b5f79d8b7382f054e
Curt Tigges <curttigges@pop-os.localdomain>
2024-01-12 17:51:11 -0800

    Started filling out readme

--- .Trash-0/files/tasks/setup-curt.sh
+++
@@ -1,3 +0,0 @@
-git config --global user.email "ct@curttigges.com" && \
-    git config --global user.name "curt-tigges" && \
-    git config --global remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
