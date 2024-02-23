207a1ff950ab238693c8c01cb6940d66a9a0fe6e
curt-tigges <ct@curttigges.com>
2024-02-23 15:16:50 -0800

    Modified get_circuits to be a parameterized script

--- get_circuits.py
+++ get_circuits.py
@@ -1,5 +1,6 @@
 #%%
 from functools import partial
+import argparse
 
 from transformer_lens import HookedTransformer
 import torch
@@ -17,7 +18,36 @@ from utils.metrics import (
     compute_logit_diff,
     compute_probability_diff,
 )
-#%%    
+#%%
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Download & assess model checkpoints")
+    parser.add_argument(
+        "-t",
+        "--task",
+        default="ioi",
+        help="Name of task dataset for which to find the circuit",
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        default="pythia-160m",
+        help="Name of model to load",
+    )
+    parser.add_argument(
+        "-e",
+        "--eval_metric",
+        default="logit_diff",
+        help="Name of metric to use for EAP evaluation",
+    )
+    parser.add_argument(
+        "-b",
+        "--batch_size",
+        default=8,
+        help="Batch size for evaluation",
+    )
+    return parser.parse_args()    
+
 
 def collate_fn(batch):
     batch_dict = {}
@@ -25,6 +55,18 @@ def collate_fn(batch):
         batch_dict[key] = torch.stack([item[key] for item in batch])
     return batch_dict
 
+def metric_mapper(metric_name):
+    if metric_name == "logit_diff":
+        return compute_logit_diff
+    elif metric_name == "prob_diff":
+        return compute_probability_diff
+    #elif metric_name == "kl_div":
+    #    return compute_kl_divergence
+    #elif metric_name == "js_div":
+    #    return compute_js_divergence
+    else:
+        raise ValueError(f"Invalid metric name: {metric_name}")
+                         
 
 def get_data_and_metrics(
         model: HookedTransformer,
@@ -61,41 +103,40 @@ def get_data_and_metrics(
 
     return ds, metric
 #%%
-batch_size = 8
-model_name = 'pythia-160m'
-model_tl_name = model_name
+def main(args):
+    model = HookedTransformer.from_pretrained(
+        args.model,
+        center_writing_weights=False,
+        center_unembed=False,
+        fold_ln=False,
+        device='cuda',
+    )
+    model.cfg.use_split_qkv_input = True
+    model.cfg.use_attn_result = True
+    model.cfg.use_hook_mlp_in = True
+    
+    # Set up for task 
+    task = args.task
+    ds, metric = get_data_and_metrics(model, task, eap=True)
+    graph = Graph.from_model(model)
+    dataloader = DataLoader(ds, batch_size=args.batch_size, collate_fn=collate_fn)
+    
+    # Evaluate baseline and graph
+    baseline = evaluate_baseline(model, dataloader, metric).mean()
+    print(baseline)
+    attribute(model, graph, dataloader, metric, integrated_gradients=30)
+    graph.apply_greedy(400)
+    graph.prune_dead_nodes(prune_childless=True, prune_parentless=True)
+    results = evaluate_graph(model, graph, dataloader, metric).mean()
+    print(results)
 
-model_full_name = f"EleutherAI/{model_name}"
-model_tl_full_name = f"EleutherAI/{model_tl_name}"
+    # Save graph and results
+    graph.to_json(f'graphs/{task}.json')
+    gz = graph.to_graphviz()
+    gz.draw(f'images/{task}.png', prog='dot')
+    return graph, results
 
-cache_dir = "~/.cache/huggingface/transformers"
+if __name__ == "__main__":
+    args = get_args()
+    main(args)
 
-model = HookedTransformer.from_pretrained(model_name,center_writing_weights=False,
-    center_unembed=False,
-    fold_ln=False,
-    device='cuda',
-)
-# %%
-task = 'ioi'
-ds, metric = get_data_and_metrics(model, task, eap=True)
-model.cfg.use_split_qkv_input = True
-model.cfg.use_attn_result = True
-model.cfg.use_hook_mlp_in = True
-graph = Graph.from_model(model)
-#%%
-dataloader = DataLoader(ds, batch_size=batch_size, collate_fn=collate_fn)
-#%%
-baseline = evaluate_baseline(model, dataloader, metric).mean()
-print(baseline)
-# %%
-attribute(model, graph, dataloader, partial(metric, loss=True), integrated_gradients=30)
-# %%
-graph.apply_greedy(400)
-graph.prune_dead_nodes(prune_childless=True, prune_parentless=True)
-results = evaluate_graph(model, graph, dataloader, metric).mean()
-print(results)
-graph.to_json(f'graphs/{task}.json')
-# %%
-gz = graph.to_graphviz()
-gz.draw(f'images/{task}.png', prog='dot')
-# %%
