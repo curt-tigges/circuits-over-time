cf362d9020d655a2e5517b32b181e173c7992693
curt-tigges <ct@curttigges.com>
2024-02-26 10:36:08 -0800

    Test debugging statement

--- utils/metrics.py
+++ utils/metrics.py
@@ -233,6 +233,8 @@ def compute_logit_diff(
     Returns:
         torch.Tensor: Difference between the logits of the provided tokens.
     """
+    print(f"positions: {positions}")
+    print(f"flags_tensor: {flags_tensor}")
     logits = get_positional_logits(logits, positions)
     
     # Mode 1: Simple
