236a8467de21802d063c66e07d9ba39f65c99100
curt-tigges <ct@curttigges.com>
2024-03-01 09:38:58 -0800

    Added new comprehensive schedule

--- get_circuits_over_time.py
+++ get_circuits_over_time.py
@@ -157,6 +157,10 @@ def get_ckpts(schedule):
             + [i * 5000 for i in range(3, 14)]
             + [i * 10000 for i in range(7, 15)]
         )
+    elif schedule == "late_start_all":
+        ckpts = (
+            [i * 1000 for i in range(1, 143)]
+        )
     else:
         ckpts = [10000, 143000]
 
