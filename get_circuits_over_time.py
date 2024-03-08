ce6a8f0a0ee8f738fb244ff0aaf0626093959160
curt-tigges <ct@curttigges.com>
2024-03-07 17:38:39 -0800

    Added various analysis notebooks

--- get_circuits_over_time.py
+++ get_circuits_over_time.py
@@ -174,7 +174,7 @@ def get_ckpts(schedule):
         )
     elif schedule == "late_start_all":
         ckpts = (
-            [i * 1000 for i in range(4, 143)]
+            [i * 1000 for i in range(4, 144)]
         )
     elif schedule == "custom":
         ckpts = []
